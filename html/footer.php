5d0a7ae0369a8f02b6d4bdb0e19f8b838a346047
IanField90 <ian.field90@gmail.com>
2011-07-07 08:44:24 +0100

    Fix SQL

--- html/footer.php
+++ html/footer.php
@@ -1,12 +1,12 @@
 <div id="footer">
 	<ul>
 		<li>
-			<a href="/SummerStudent/./html/content/terms.php">
+			<a href="/SummerStudent/html/content/terms.php">
 				Terms and Conditions
 			</a>
 		</li>
 		<li>
-			<a href="/SummerStudent/./html/content/privacy.php">
+			<a href="/SummerStudent/html/content/privacy.php">
 				Privacy Policy
 			</a>
 		</li>
--- html/navigation_bar.php
+++ html/navigation_bar.php
@@ -1,10 +1,10 @@
 <div id="nav">
 	<ul>
 		<li>
-			<a href="/SummerStudent/./html" class="<? echo $sel_home; ?>">Home</a>
+			<a href="/SummerStudent/html" class="<? echo $sel_home; ?>">Home</a>
 		</li>
 		<li>
-			<a href="/SummerStudent/./html/About" class="<? echo $sel_about; ?>">About</a>
+			<a href="/SummerStudent/html/About" class="<? echo $sel_about; ?>">About</a>
 		</li>
 	</ul>
 
