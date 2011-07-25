00bf216d54111aebb7ae5db6611b1d4d87222f02
IanField90 <ian.field90@gmail.com>
2011-07-25 14:45:33 +0100

    Link clean up

--- html/footer.php
+++ html/footer.php
@@ -2,17 +2,17 @@
 <div id="footer">
 	<ul>
 		<li>
-			<a href="/SummerStudent/html" class="<?php echo $sel_home; ?>">
+			<a href="/" class="<?php echo $sel_home; ?>">
 				<?php echo $link_home; ?>
 			</a>
 		</li>
 		<li>
-			<a href="/SummerStudent/html/About" class="<?php echo $sel_about; ?>">
+			<a href="/about" class="<?php echo $sel_about; ?>">
 				<?php echo $link_about; ?>
 			</a>
 		</li>
 		<li>
-			<a href="/SummerStudent/html/content/privacy.php">
+			<a href="/content/privacy.php">
 				<?php echo $privacy_page_title; ?>
 			</a>
 		</li>
